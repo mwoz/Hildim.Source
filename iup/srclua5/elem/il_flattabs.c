03ba7a2c3e219066bcf79862e2b54e85db98c4d1
mwoz <mwoz@mail.ru>
2018-01-25 07:24:50 +0300

    переводы строк

--- iup/srclua5/elem/il_flattabs.c
+++ iup/srclua5/elem/il_flattabs.c
@@ -1,40 +1,40 @@
-/******************************************************************************
- * Automatically generated file. Please don't change anything.                *
- *****************************************************************************/
-
-#include <stdlib.h>
-
-#include <lua.h>
-#include <lauxlib.h>
-
-#include "iup.h"
-#include "iuplua.h"
-#include "il.h"
-
-
-static int FlatTabs(lua_State *L)
-{
-  Ihandle *ih = IupFlatTabs(NULL);
-  iuplua_plugstate(L, ih);
-  iuplua_pushihandle_raw(L, ih);
-  return 1;
-}
-
-int iupflattabslua_open(lua_State * L)
-{
-  iuplua_register(L, FlatTabs, "FlatTabs");
-
-
-#ifdef IUPLUA_USELOH
-#include "flattabs.loh"
-#else
-#ifdef IUPLUA_USELH
-#include "flattabs.lh"
-#else
-  iuplua_dofile(L, "flattabs.lua");
-#endif
-#endif
-
-  return 0;
-}
-
+/******************************************************************************
+ * Automatically generated file. Please don't change anything.                *
+ *****************************************************************************/
+
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#include "iup.h"
+#include "iuplua.h"
+#include "il.h"
+
+
+static int FlatTabs(lua_State *L)
+{
+  Ihandle *ih = IupFlatTabs(NULL);
+  iuplua_plugstate(L, ih);
+  iuplua_pushihandle_raw(L, ih);
+  return 1;
+}
+
+int iupflattabslua_open(lua_State * L)
+{
+  iuplua_register(L, FlatTabs, "FlatTabs");
+
+
+#ifdef IUPLUA_USELOH
+#include "flattabs.loh"
+#else
+#ifdef IUPLUA_USELH
+#include "flattabs.lh"
+#else
+  iuplua_dofile(L, "flattabs.lua");
+#endif
+#endif
+
+  return 0;
+}
+
