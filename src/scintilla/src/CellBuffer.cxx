944814d73eedc413132af78ad5f8114480768f5d
mwoz <mwoz@mail.ru>
2019-10-30 09:12:28 +0300

    Накат моих изменений. Версия 4.3.0

--- src/scintilla/src/CellBuffer.cxx
+++ src/scintilla/src/CellBuffer.cxx
@@ -139,14 +139,14 @@ class LineVector : public ILineVector {
 	LineStartIndex<POS> startsUTF32;
 public:
 	LineVector() : starts(256), perLine(nullptr) {
-	}
+ 	}
 	// Deleted so LineVector objects can not be copied.
 	LineVector(const LineVector &) = delete;
 	LineVector(LineVector &&) = delete;
 	LineVector &operator=(const LineVector &) = delete;
 	LineVector &operator=(LineVector &&) = delete;
 	~LineVector() override {
-	}
+ 	}
 	void Init() override {
 		starts.DeleteAll();
 		if (perLine) {
@@ -423,7 +423,10 @@ void UndoHistory::BeginUndoAction() {
 	undoSequenceDepth++;
 }
 
-void UndoHistory::EndUndoAction() {
+int UndoHistory::EndUndoAction() {
+	int res = undoSequenceDepth;
+	if (!res)
+		return res;
 	PLATFORM_ASSERT(undoSequenceDepth > 0);
 	EnsureUndoRoom();
 	undoSequenceDepth--;
@@ -435,6 +438,7 @@ void UndoHistory::EndUndoAction() {
 		}
 		actions[currentAction].mayCoalesce = false;
 	}
+	return res;
 }
 
 void UndoHistory::DropUndoSequence() {
@@ -679,7 +683,7 @@ void CellBuffer::Allocate(Sci::Position newSize) {
 }
 
 void CellBuffer::SetUTF8Substance(bool utf8Substance_) {
-	utf8Substance = utf8Substance_;
+		utf8Substance = utf8Substance_;
 }
 
 void CellBuffer::SetLineEndTypes(int utf8LineEnds_) {
@@ -1142,8 +1146,8 @@ void CellBuffer::BeginUndoAction() {
 	uh.BeginUndoAction();
 }
 
-void CellBuffer::EndUndoAction() {
-	uh.EndUndoAction();
+int CellBuffer::EndUndoAction() {
+	return uh.EndUndoAction();
 }
 
 void CellBuffer::AddUndoAction(Sci::Position token, bool mayCoalesce) {
