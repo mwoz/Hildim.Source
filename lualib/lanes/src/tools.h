e8e03787e7ec5e57df9c264976a676623e9c3419
mwoz <mwoz@mail.ru>
2021-07-29 15:08:16 +0300

    lua 543+ обновление lualanes

--- lualib/lanes/src/tools.h
+++ lualib/lanes/src/tools.h
@@ -24,8 +24,18 @@ HWND m_hwnd;
 void luaG_dump( lua_State* L);
 #endif // _DEBUG
 
-lua_State* create_state( Universe* U, lua_State* from_);
-lua_State* luaG_newstate( Universe* U, lua_State* _from, char const* libs);
+// ################################################################################################
+
+void push_registry_subtable_mode( lua_State* L, UniqueKey key_, const char* mode_);
+void push_registry_subtable( lua_State* L, UniqueKey key_);
+
+enum e_vt
+{
+	VT_NORMAL,
+	VT_KEY,
+	VT_METATABLE
+};
+bool_t inter_copy_one( Universe* U, lua_State* L2, uint_t L2_cache_i, lua_State* L, uint_t i, enum e_vt vt, LookupMode mode_, char const* upName_);
 
 // ################################################################################################
 
@@ -41,9 +51,6 @@ void populate_func_lookup_table( lua_State* L, int _i, char const* _name);
 void serialize_require( DEBUGSPEW_PARAM_COMMA( Universe* U) lua_State *L);
 void initialize_allocator_function( Universe* U, lua_State* L);
 void cleanup_allocator_function( Universe* U, lua_State* L);
-
-void initialize_on_state_create( Universe* U, lua_State* L);
-void call_on_state_create( Universe* U, lua_State* L, lua_State* from_, LookupMode mode_);
 static int hildim_print(const char* msg);
 
 // ################################################################################################
